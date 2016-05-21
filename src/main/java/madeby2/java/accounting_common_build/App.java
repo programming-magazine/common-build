afcbf08f67c04abf173fca33e1194d2510b6f9d4
Stephen <development.tutorialsmag@gmail.com>
2016-05-21 16:03:35 +0100

    Initial Commit

---
+++ src/main/java/madeby2/java/accounting_common_build/App.java
@@ -0,0 +1,13 @@
+package madeby2.java.accounting_common_build;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
